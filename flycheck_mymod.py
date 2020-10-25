5b87f536c8c2737ddc2cdf7dbe7d0e6db1f697b2
Reuven Lerner <reuven@lerner.co.il>
2020-10-25 10:10:17 +0200

    gitautosync update 21

---
+++ flycheck_mymod.py
@@ -0,0 +1,15 @@
+# __all__ = ['x', 'hello']
+__all__ = ['YOU_SHOULD_NEVER_USE_IMPORT_*!']
+
+print
+
+x = 100
+
+y = [10, 20, 30]
+
+
+def hello(name):
+    return f'Hello, {name}, from mymod!'
+
+
+_secret = 'shhh!'
